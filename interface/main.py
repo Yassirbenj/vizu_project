fe1f9354c684dfb66ab4f438f0a5ea2f6d7ee719
yassir benjilali <benjilaliyassir@forleaders.net>
2023-09-08 19:17:45 +0100

    boutons infos et stat

--- interface/main.py
+++ interface/main.py
@@ -3,14 +3,15 @@ import matplotlib.pyplot as plt
 import seaborn as sn
 import streamlit as st
 
-with st.form("input_form",clear_on_submit=True):
+with st.form("input_form",clear_on_submit=False):
     st.write("<h3>Upload your data on csv format ✨</h3>", unsafe_allow_html=True)
     data=st.file_uploader('download your csv file',type='csv')
+    if data is not None:
+        df=pd.read_csv(data)
+        st.dataframe(df)
     if st.form_submit_button("Basic infos"):
         if data is not None:
-            df=pd.read_csv(data)
             st.text(df.info())
     if st.form_submit_button("Basic statistics"):
         if data is not None:
-            df=pd.read_csv(data)
             st.text(df.describe())
