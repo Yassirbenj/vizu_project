774e88a78383740e8cb965c0610513a8c9512286
yassir benjilali <benjilaliyassir@forleaders.net>
2023-09-08 19:05:49 +0100

    boutons infos et stat

--- interface/main.py
+++ interface/main.py
@@ -6,7 +6,11 @@ import streamlit as st
 with st.form("input_form",clear_on_submit=True):
     st.write("<h3>Upload your data on csv format ✨</h3>", unsafe_allow_html=True)
     data=st.file_uploader('download your csv file',type='csv')
-    if st.form_submit_button("Predict"):
+    if st.form_submit_button("Basic infos"):
         if data is not None:
             df=pd.read_csv(data)
-            st.text(df.head())
+            st.text(df.infos())
+    if st.form_submit_button("Basic statistics"):
+        if data is not None:
+            df=pd.read_csv(data)
+            st.text(df.infos())
