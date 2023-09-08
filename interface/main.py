c5fd3c2eb722791c9482b5ca57861538f8bb99f4
yassir benjilali <benjilaliyassir@forleaders.net>
2023-09-08 19:25:58 +0100

    boutons infos et stat

--- interface/main.py
+++ interface/main.py
@@ -7,10 +7,10 @@ with st.form("input_form",clear_on_submit=False):
     st.write("<h3>Upload your data on csv format ✨</h3>", unsafe_allow_html=True)
     data=st.file_uploader('download your csv file',type='csv')
     if data is not None:
-        df=pd.read_csv(data.head())
-        st.dataframe(df)
+        df=pd.read_csv(data)
     if st.form_submit_button("Basic infos"):
         if data is not None:
+            st.dataframe(df.head())
             st.dataframe(df.info())
     if st.form_submit_button("Basic statistics"):
         if data is not None:
