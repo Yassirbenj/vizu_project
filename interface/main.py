cf669c9806e0a8b8b24e1c82ffa554e4badf6cd4
yassir benjilali <benjilaliyassir@forleaders.net>
2023-09-08 19:37:40 +0100

    boutons infos et stat

--- interface/main.py
+++ interface/main.py
@@ -10,8 +10,11 @@ with st.form("input_form",clear_on_submit=False):
         df=pd.read_csv(data)
     if st.form_submit_button("Basic infos"):
         if data is not None:
+            st.text(f'<h4> Your table contain {len(df.index)} rows. Below a view of the first 5 lines of your table </h4>',unsafe_allow_html=True)
             st.dataframe(df.head())
+            st.text("<h4>Below the types of each column </h4>",unsafe_allow_html=True)
             st.dataframe(df.dtypes)
     if st.form_submit_button("Basic statistics"):
         if data is not None:
+            st.text("<h4>Below basic statistics of each column </h4>",unsafe_allow_html=True)
             st.dataframe(pd.DataFrame(df.describe()))
